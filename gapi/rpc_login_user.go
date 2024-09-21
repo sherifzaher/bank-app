1e0b70fe5cf830d189e91bf7bf4e6fb797882650
sherifzaher <sherifashrafmedia@gmail.com>
2024-09-21 05:20:48 +0300

    add grpc validations to request params

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -6,12 +6,19 @@ import (
 	db "github.com/sherifzaher/clone-simplebank/db/sqlc"
 	"github.com/sherifzaher/clone-simplebank/pb"
 	"github.com/sherifzaher/clone-simplebank/util"
+	"github.com/sherifzaher/clone-simplebank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -58,4 +65,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	return violations
 }
