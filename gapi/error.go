56243012b2adc24087a54777b29d234da3d20562
sherifzaher <sherifashrafmedia@gmail.com>
2024-09-21 07:02:32 +0300

    add authorization layer to gRPC gateway and make it run with gRPC service and HTTP gateway

--- gapi/error.go
+++ gapi/error.go
@@ -23,3 +23,7 @@ func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) er
 	}
 	return statusDetails.Err()
 }
+
+func unAuthenticatedError(err error) error {
+	return status.Errorf(codes.Unauthenticated, "unauthorized: %s", err)
+}
